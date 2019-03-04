c00177858e2dd45f108cde9bedbdf15c118d56d9
Jonathan Soma <jonathansoma@skidbladnir.local>
2019-03-04 09:10:42 -0500

    improved about page

--- src/renderer/components/AboutView.vue
+++ src/renderer/components/AboutView.vue
@@ -3,8 +3,11 @@
     <h1 class="title">About Python Wrangler</h1>
     <div class="content">
       <p>Python Wrangler was made by <a href="http://jonathansoma.com/">Jonathan Soma</a> under the guise of <a href="http://littlecolumns.com/">Little Columns</a>, because installing Python is remarkably un-fun.</p>
-      <p>More details <a href="http://littlecolumns.com/tools/python-wrangler/">on the Little Columns site</a>, or peruse the code in the <a href="https://github.com/littlecolumns/python-wrangler">GitHub repo</a>.
-      <p>Super sweet snake icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, and is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>.</p>
+      <p>More details <a href="http://littlecolumns.com/tools/python-wrangler/">on the Little Columns site</a>, or peruse the code in the <a href="https://github.com/littlecolumns/python-wrangler">GitHub repo</a>.</p>
+      <h3>Having trouble?</h3>
+      <p>Feel free to reach out at <strong>hello@littlecolumns.com</strong> or <a href="https://twitter.com/dangerscarf">@dangerscarf</a>. To help me debug your situation, click the <strong>Export</strong> button on the sidebar and include that text file as an attachment.</p>
+      <h3>Acknowledgements</h3>
+      <p>Super sweet snake icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, and is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>. And to all my poor, poor, students who were strong-armed into beta testing this.</p>
     </div>
   </div>
 </template>
